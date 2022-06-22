3798734c2171d79ad97c2d04cbf525aa9b0f2a9f
Bùi Dương Thế <buiduongthe@gmail.com>
2022-06-22 16:24:23 +0700

    update backend layout, login, theme

---
+++ backend/views/layouts/main-login.php
@@ -0,0 +1,39 @@
+<?php
+
+/* @var $this View */
+/* @var $content string */
+
+use yii\web\View;
+
+\hail812\adminlte3\assets\AdminLteAsset::register($this);
+$this->registerCssFile('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700');
+$this->registerCssFile('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css');
+\hail812\adminlte3\assets\PluginAsset::register($this)->add(['fontawesome', 'icheck-bootstrap']);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?=$this->title = "NEST SYSTEM LOGIN"?></title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <?php $this->head() ?>
+</head>
+<body class="hold-transition login-page">
+<?php  $this->beginBody() ?>
+<div class="login-box">
+    <div class="login-logo">
+        <a href="<?=Yii::$app->homeUrl?>"><img src="/images/logo.svg"></a>
+    </div>
+    <!-- /.login-logo -->
+    <?= $content ?>
+</div>
+<!-- /.login-box -->
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
